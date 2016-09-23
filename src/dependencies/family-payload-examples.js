47e975205a5f895701c722670b7a3cd31ae7775a
shawnmarie <shawn.abbott@versionone.com>
2016-09-23 15:09:20 -0400

    updated demo data files for SVN and GitSwarm

--- src/dependencies/family-payload-examples.js
+++ src/dependencies/family-payload-examples.js
@@ -400,5 +400,83 @@ export default {
 			  "createdDate": "2015-11-11T20:13:52.4966577Z"
 			}
 		)
+	},
+	SVN: {
+		validWithOneCommit: message => (
+		{
+		  "repository": "http://v1commitstream.cloudapp.net:9090/svn/ProjectA",
+		  "committer": {
+		    "name": "admin",
+		    "date": "2016-09-12 03:52:55 +1300 (Mon, 16 Sep 2016)"
+		  },
+		  "pretext": "Commit completed:  rev. 12",
+		  "revision": "12",
+		  "author": "admin",
+		  "changes": [
+		    "U   mariano.txt"
+		  ],
+		  "message": message,
+		  "html_url": "http://v1commitstream.cloudapp.net:9090/!/#ProjectA/commit/r12"
+		})
+	},
+	GitSwarm: {
+		validWithOneCommit: message => (
+			{
+		    "object_kind": "push",
+		    "event_name": "push",
+		    "before": "bf2c20bc7824a3da6bda5b301560cc0bdfe1b557",
+		    "after": "6f1268102193085e512b3e13a701d201ce522e85",
+		    "ref": "refs/heads/master",
+		    "checkout_sha": "6f1268102193085e512b3e13a701d201ce522e85",
+		    "message": null,
+		    "user_id": 1,
+		    "user_name": "Administrator",
+		    "user_email": "admin@example.com",
+		    "user_avatar": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+		    "project_id": 1,
+		    "project": {
+		        "name": "api-testing",
+		        "description": "API testing with Postman",
+		        "web_url": "http://gitswarm.cloudapp.net/root/api-testing",
+		        "avatar_url": null,
+		        "git_ssh_url": "git@gitswarm.cloudapp.net:root/api-testing.git",
+		        "git_http_url": "http://gitswarm.cloudapp.net/root/api-testing.git",
+		        "namespace": "root",
+		        "visibility_level": 0,
+		        "path_with_namespace": "root/api-testing",
+		        "default_branch": "master",
+		        "homepage": "http://gitswarm.cloudapp.net/root/api-testing",
+		        "url": "git@gitswarm.cloudapp.net:root/api-testing.git",
+		        "ssh_url": "git@gitswarm.cloudapp.net:root/api-testing.git",
+		        "http_url": "http://gitswarm.cloudapp.net/root/api-testing.git"
+		    },
+		    "commits": [
+		        {
+		            "id": "6f1268102193085e512b3e13a701d201ce522e85",
+		            "message": message,
+		            "timestamp": "2016-09-22T11:21:18+00:00",
+		            "url": "http://gitswarm.cloudapp.net/root/api-testing/commit/6f1268102193085e512b3e13a701d201ce522e85",
+		            "author": {
+		                "name": "Administrator",
+		                "email": "admin@example.com"
+		            },
+		            "added": [],
+		            "modified": [
+		                "ReadMe2.md"
+		            ],
+		            "removed": []
+		        }
+		    ],
+		    "total_commits_count": 1,
+		    "repository": {
+		        "name": "api-testing",
+		        "url": "git@gitswarm.cloudapp.net:root/api-testing.git",
+		        "description": "API Testing with Postman",
+		        "homepage": "http://gitswarm.cloudapp.net/root/api-testing",
+		        "git_http_url": "http://gitswarm.cloudapp.net/root/api-testing.git",
+		        "git_ssh_url": "git@gitswarm.cloudapp.net:root/api-testing.git",
+		        "visibility_level": 0
+		    }
+		})
 	}
 };
