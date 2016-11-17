b5edb4011f29d6bc31a225d782995c1ca5325262
shawnmarie <shawn.abbott@versionone.com>
2016-11-17 08:41:41 -0500

    added lodash as dev dependency, UI test files now have the ability read and write to inboxes.json file

--- src/tests/commitStreamCustomUI.js
+++ src/tests/commitStreamCustomUI.js
@@ -2,6 +2,9 @@ import Glance from "glance-webdriver";
 import test from 'ava';
 import chai from 'chai';
 import selenium from '../dependencies/selenium';
+import fs from 'fs';
+import _ from 'lodash';
+
 chai.should();
 
 var glance;
@@ -25,11 +28,22 @@ test.before("Start selenium and init glance", t => {
     });
 });
 
+test.after('saveInboxes', t=> {
+    fs.writeFileSync('../../config/inboxes.json',JSON.stringify(inboxes, null, "\t"));
+});
 test.after.always("Cleanup", t => {
     return glance.end().then(selenium.stop)
 });
 
 let title;
+let inboxesJSON = fs.readFileSync('../../config/inboxes.json', 'utf-8');
+let inboxes = JSON.parse(inboxesJSON);
+
+let changeInboxWebhook=(family, webhook) => {
+    let vcsInbox = _.findLast(inboxes, {'family': family});
+    vcsInbox['_links']['add-commit']['href']=webhook;
+}
+
 
 test("Login to VersionOne-SMA Instance", t=> {
     return glance.url("http://localhost/VersionOne")
@@ -43,10 +57,10 @@ test("Login to VersionOne-SMA Instance", t=> {
         //.click('Back to Main')
         //.click('My Home')
         //.click('Getting Started')
-        .get("page-title").then(function(title) {
-            console.log('Title was: ' + title);
-            t.is(title, "Let's Get Started", "This test was an utter failure.")
-        })
+        // .get("page-title").then(function(title) {
+        //     console.log('Title was: ' + title);
+        //     t.is(title, "Let's Get Started", "This test was an utter failure.")
+        // })
         .click('ADMIN#1')
         .click('DEVOPS')
         .click('CommitStream')
@@ -68,10 +82,13 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('h1#2').then(function(header) {
             console.log('Table header was: ', header);
             t.is(header, "Active Repositories", "What value did I get then?: " + header)
+            return glance
         })
         .get('genericTest>input').then(function(gitHubUrl) {
             console.log("GitHub URL is: ", gitHubUrl);
             gitHubUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + gitHubUrl);
+            changeInboxWebhook('GitHub', gitHubUrl);
+            return glance
         })
         .click('GitLab')
         .set('inboxUrl>input', 'https://gitlab.com/user/localFox')
@@ -80,10 +97,13 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('h1#2').then(function(header) {
             console.log('Table header was: ', header);
             t.is(header, "Active Repositories", "What value did I get then?: " + header)
+            return glance
         })
         .get('localFox>input').then(function(gitLabUrl) {
             console.log("GitLab URL is: ", gitLabUrl);
             gitLabUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + gitLabUrl);
+            changeInboxWebhook('GitLab', gitLabUrl);
+            return glance
         })
         .click('Bitbucket')
         .set('inboxUrl>input', 'https://bitbucket.org/user/blueTub')
@@ -92,10 +112,13 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('h1#2').then(function(header) {
             console.log('Table header was: ', header);
             t.is(header, "Active Repositories", "What value did I get then?: " + header)
+            return glance
         })
         .get('blueTub>input').then(function(bitBucketUrl) {
             console.log("Bitbucket URL is: ", bitBucketUrl);
             bitBucketUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + bitBucketUrl);
+            changeInboxWebhook('Bitbucket', bitBucketUrl);
+            return glance
         })
         .click('VSTS')
         .set('inboxUrl>input', 'https://microsoft.vsogit.com/user/microSoft')
@@ -104,10 +127,13 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('h1#2').then(function(header) {
             console.log('Table header was: ', header);
             t.is(header, "Active Repositories", "What value did I get then?: " + header)
+            return glance
         })
         .get('microSoft>input').then(function(vsoGitUrl) {
             console.log("VSTS URL is: ", vsoGitUrl);
             vsoGitUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + vsoGitUrl);
+            changeInboxWebhook('VsoGit', vsoGitUrl);
+            return glance
         })
         .click('Subversion')
         .set('inboxUrl>input', 'https://subversion.com/user/depotNum1')
@@ -116,31 +142,33 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('h1#2').then(function(header) {
             console.log('Table header was: ', header);
             t.is(header, "Active Repositories", "What value did I get then?: " + header)
+            return glance
         })
         .get('depotNum1>input').then(function(svnRepoUrl) {
             console.log("SVN URL is: ", svnRepoUrl);
             svnRepoUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + svnRepoUrl);
+            changeInboxWebhook('SVN', svnRepoUrl);
+            return glance
         })
         //clean up
-        .click('genericTest>iconRemove')
-        .click('modal>button^Remove')
-        .click('localFox>iconRemove')
-        .click('modal>button^Remove')
-        .click('blueTub>iconRemove')
-        .click('modal>button^Remove')
-        .click('microSoft>iconRemove')
-        .click('modal>button^Remove')
-        .click('depotNum1>iconRemove')
-        .click('modal>button^Remove')
-        .click('Enabled')
-        //.click('Back to Main')
-        .click('utility-bar>Main')
-        .click('My Home')
-        .click('Getting Started')
-        .get("page-title").then(function(title) {
-             console.log('Title was: ' + title);
-             t.is(title, "Let's Get Started", "This test was an utter failure.")
-        })
-    //.end();
+        // .click('genericTest>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('localFox>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('blueTub>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('microSoft>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('depotNum1>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('Enabled')
+        // //.click('Back to Main')
+        // .click('utility-bar>Main')
+        // .click('My Home')
+        // .click('Getting Started')
+        // .get("page-title").then(function(title) {
+        //      console.log('Title was: ' + title);
+        //      t.is(title, "Let's Get Started", "This test was an utter failure.")
+        // })
 });
 
--- src/tests/commitStreamGlobalUI.js
+++ src/tests/commitStreamGlobalUI.js
@@ -2,6 +2,8 @@ import Glance from "glance-webdriver";
 import test from 'ava';
 import chai from 'chai';
 import selenium from '../dependencies/selenium';
+import fs from 'fs';
+import _ from 'lodash';
 chai.should();
 
 var glance;
@@ -24,12 +26,21 @@ test.before("Start selenium and init glance", t => {
         });
     });
 });
-
+test.after('saveInboxes', t=> {
+    fs.writeFileSync('../../config/inboxes.json',JSON.stringify(inboxes, null, "\t"));
+});
 test.after.always("Cleanup", t => {
     return glance.end().then(selenium.stop)
 });
 
 let title;
+let inboxesJSON = fs.readFileSync('../../config/inboxes.json', 'utf-8');
+let inboxes = JSON.parse(inboxesJSON);
+
+let changeInboxWebhook=(family, webhook) => {
+    let vcsInbox = _.find(inboxes, {'family': family});
+    vcsInbox['_links']['add-commit']['href']=webhook;
+}
 
 test("Login to VersionOne-SMA Instance", t=> {
     return glance.url("http://localhost/VersionOne/")
@@ -47,11 +58,11 @@ test("Login to VersionOne-SMA Instance", t=> {
         //     return glance
         // })
         .click('ADMIN#1')
-        .pause(4000)
+        .pause(1000)
         .click('DevOps')
-        .pause(3000)
+        .pause(1000)
         .click('CommitStream')
-        .pause(3000)
+        .pause(1000)
         .click('Disabled')
         .pause(2000)
         .click('GitHub')
@@ -66,6 +77,7 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('genericTest>input').then(function(gitHubUrl) {
             console.log("GitHub URL is: ", gitHubUrl);
             gitHubUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + gitHubUrl);
+            changeInboxWebhook('GitHub', gitHubUrl);
             return glance
         })
         .click('GitLab')
@@ -80,6 +92,7 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('localFox>input').then(function(gitLabUrl) {
                 console.log("GitLab URL is: ", gitLabUrl);
                 gitLabUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + gitLabUrl);
+                changeInboxWebhook('GitLab', gitLabUrl);
                 return glance
             })
         .click('Bitbucket')
@@ -94,6 +107,7 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('blueTub>input').then(function(bitBucketUrl) {
              console.log("Bitbucket URL is: ", bitBucketUrl);
              bitBucketUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + bitBucketUrl);
+             changeInboxWebhook('Bitbucket', bitBucketUrl);
              return glance
              })
         .click('VSTS')
@@ -108,6 +122,7 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('microSoft>input').then(function(vsoGitUrl) {
             console.log("VSTS URL is: ", vsoGitUrl);
             vsoGitUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + vsoGitUrl);
+            changeInboxWebhook('VsoGit', vsoGitUrl);
             return glance
             })
         .click('Subversion')
@@ -122,27 +137,44 @@ test("Login to VersionOne-SMA Instance", t=> {
         .get('depotNum1>input').then(function(svnRepoUrl) {
             console.log("SVN URL is: ", svnRepoUrl);
             svnRepoUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + svnRepoUrl);
+            changeInboxWebhook('SVN', svnRepoUrl);
+            return glance
+        })
+        .click('GitSwarm')
+        .set('inboxUrl>input', 'http://gitswarm.com/company/globalSwarm')
+        .click('Add')
+        .pause(2000)
+        .get('h1#2').then(function(header) {
+            console.log('Table header was: ', header);
+            t.is(header, "Active Repositories", "What value did I get then?: " + header);
+            return glance
+        })
+        .get('globalSwarm>input').then(function(gitSwarmUrl) {
+            console.log("GitSwarm URL is: ", gitSwarmUrl);
+            gitSwarmUrl.should.include('https://v1-cs-test.azurewebsites.net/api/', "This should be an inbox URL: " + gitSwarmUrl);
+            changeInboxWebhook('GitSwarm', gitSwarmUrl);
             return glance
         })
          //clean up
-        .click('genericTest>iconRemove')
-        .click('modal>button^Remove')
-        .click('localFox>iconRemove')
-        .click('modal>button^Remove')
-        .click('blueTub>iconRemove')
-        .click('modal>button^Remove')
-        .click('microSoft>iconRemove')
-        .click('modal>button^Remove')
-        .click('depotNum1>iconRemove')
-        .click('modal>button^Remove')
-        .click('Enabled')
-        .click('Back to Main')
-        .click('My Home')
-        .click('Getting Started')
-        .get("page-title").then(function(title) {
-            console.log('Title was: ' + title);
-            t.is(title, "Let's Get Started", "This test was an utter failure.")
-        })
-        //.end();
+        // .click('genericTest>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('localFox>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('blueTub>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('microSoft>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('depotNum1>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('globalSwarm>iconRemove')
+        // .click('modal>button^Remove')
+        // .click('Enabled')
+        // .click('Back to Main')
+        // .click('My Home')
+        // .click('Getting Started')
+        // .get("page-title").then(function(title) {
+        //     console.log('Title was: ' + title);
+        //     t.is(title, "Let's Get Started", "This test was an utter failure.")
+        // })
 });
 
